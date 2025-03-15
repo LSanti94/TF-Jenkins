badbc0102a4558e8071c739280ec6bd5a375ca05
LSanti94 <luissantillan1994@hotmail.com>
2025-03-15 18:44:23 -0500

    update TF

--- main.tf
+++ main.tf
@@ -3,8 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0eae2a0fc13b15fce"
-  instance_type = "t2.micro"
+  ami                    = "ami-0eae2a0fc13b15fce"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0e768ed6dc6ac2f95"]
+  subnet_id              = "subnet-0d60de508ecf74c25"
   tags = {
     Name = "TF-Instance"
   }
