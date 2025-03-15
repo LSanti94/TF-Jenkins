ed10c0f6d344612a4948076548ff7af199267e0d
LSanti94 <luissantillan1994@hotmail.com>
2025-03-15 18:47:19 -0500

    update TF

--- main.tf
+++ main.tf
@@ -2,12 +2,53 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
-  ami                    = "ami-0eae2a0fc13b15fce"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-0e768ed6dc6ac2f95"]
-  subnet_id              = "subnet-0d60de508ecf74c25"
+
+# Crear una VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "TF-Instance"
+    Name = "MyVPC"
+  }
+}
+
+# Crear una subred dentro de la VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a" # Reemplaza por la zona de tu preferencia
+}
+
+# Crear un grupo de seguridad dentro de la VPC
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+# Crear una instancia EC2 dentro de la VPC y Subnet
+resource "aws_instance" "foo" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  subnet_id              = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "MyInstance"
   }
 }
