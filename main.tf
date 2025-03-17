937cc994ba1aa57000fb2d080cf1cf5ad23b45b6
LSanti94 <luissantillan1994@hotmail.com>
2025-03-16 19:46:06 -0500

    update TF

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
   tags = {
     Name = "my-subnet"
   }
@@ -59,11 +59,12 @@ resource "aws_security_group" "my_security_group" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami                         = "ami-08b5b3a93ed654d19" # Reemplaza con el AMI que necesitas
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.my_subnet.id
-  vpc_security_group_ids      = [aws_security_group.my_security_group.id]
+  ami                    = "ami-08b5b3a93ed654d19" # Reemplaza con el AMI que necesitas
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
   tags = {
     Name = "my-instance"
   }
 }
+
